701556a755e445ebcde3c9d3763041946e990c5d
tedyswaga <teddymutuma@gmail.com>
2018-07-22 16:45:17 +0300

    update with searc

---
+++ src/components/Scroll.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+
+
+const Scroll =(props) =>{
+
+return(
+
+	<div style={{overflow: 'scroll', border:'2px solid white', height: '500px'}}>
+
+	{props.children}
+
+	</div>
+	
+	)
+
+};
+
+export default Scroll;
